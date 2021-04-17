cf47309e2dd632d3511b8a51b717ffd39a694361
JenkJS <jenkjobs@gmail.com>
2021-04-17 23:03:20 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Categories({ items }) {
+  const [activeItem, setActiveItem] = React.useState(null);
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        <li className={activeItem === null ? "active" : ""}>Все</li>
+        {items.map((name, index) => (
+          <li
+            className={activeItem === index ? "active" : ""}
+            onClick={() => onSelectItem(index)}
+            key={`${name}_${index}`}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
